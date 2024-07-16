c6bcd87604255b62e1be9da294a3c631d8ac16e6
Sudharsan Regmi <shudarsanregmi.work@gmail.com>
2024-07-16 18:34:11 +0530

    Feature to add and remove list item was added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,17 +19,20 @@ void MainWindow::on_addBtn_clicked()
     QListWidgetItem* item = new QListWidgetItem(ui->textBox->text(), ui->listWidget);
     ui->listWidget->addItem(item);
     ui->textBox->clear();
+    ui->textBox->setFocus();
+    item->setFlags(item->flags() | Qt::ItemIsEditable);
 }
 
 
 void MainWindow::on_removeBtn_clicked()
 {
+    QListWidgetItem* item = ui->listWidget->takeItem(ui->listWidget->currentRow());
+    delete item;
 
 }
 
 
 void MainWindow::on_removeAllBtn_clicked()
 {
-
+    ui->listWidget->clear();
 }
-
